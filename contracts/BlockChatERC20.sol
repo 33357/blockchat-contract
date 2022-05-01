590c7b5f656bea788b6bc9bf6050fbafdfafa202
33357 <1043898730@qq.com>
2022-05-01 13:45:29 +0100

    update BlockChatERC20

--- contracts/BlockChatERC20.sol
+++ contracts/BlockChatERC20.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.12;
 
 import "./interfaces/IBlockChatUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract BlockChatERC20 is ERC20 {
-
     IBlockChatUpgradeable public blockChat = IBlockChatUpgradeable(0x21f4463D28c2921c34063D676d9Cefb159820aed);
 
-    constructor() ERC20("BlockChatERC20", "BC") {}
+    constructor() ERC20("BlockChatToken", "BCT") {
+        _mint(msg.sender, 10**8 * 10**18);
+    }
 
     /* ================ UTIL FUNCTIONS ================ */
 
@@ -19,7 +21,7 @@ contract BlockChatERC20 is ERC20 {
     ) internal override {
         blockChat.createMessage(
             bytes20(address(this)),
-            string(abi.encodePacked("transfer ", symbol(), " from ", from, " to ", to, " amount ", amount))
+            string(abi.encodePacked("transfer ", symbol(), " from ", Strings.toHexString(uint160(from), 20), " to ", Strings.toHexString(uint160(to), 20), " amount ", Strings.toString(amount)))
         );
     }
 }
